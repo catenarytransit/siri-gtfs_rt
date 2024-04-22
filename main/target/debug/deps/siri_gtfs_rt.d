83081720152a959490da9d6975ea2e439fa30812
Nebula3lem123 <whitecottonjacob00@gmail.com>
2024-04-21 18:32:41 -0600

    rename inner file to main

---
+++ main/target/debug/deps/siri_gtfs_rt.d
@@ -0,0 +1,5 @@
+C:\Users\white\dev\catenary\siri-gtfs_rt\target\debug\deps\siri_gtfs_rt.exe: src\main.rs
+
+C:\Users\white\dev\catenary\siri-gtfs_rt\target\debug\deps\siri_gtfs_rt.d: src\main.rs
+
+src\main.rs:
---
+++ main/target/debug/siri-gtfs_rt.d
@@ -0,0 +1,1 @@
+C:\Users\white\dev\catenary\siri-gtfs_rt\target\debug\siri-gtfs_rt.exe: C:\Users\white\dev\catenary\siri-gtfs_rt\src\main.rs
