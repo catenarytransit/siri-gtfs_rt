791f095092755acef0fcdda5a40d326b5b43b57e
Nebula3lem123 <whitecottonjacob00@gmail.com>
2024-04-30 19:19:34 -0600

    updated siri structs, gitignore is being stupid so help needed with that

--- src/lib.rs
+++ src/lib.rs
@@ -9,92 +9,93 @@ pub fn get_gtfs_rt() -> Result<gtfs_rt::FeedMessage, Box<dyn std::error::Error +
     )))
 }
 
+
 #[derive(Deserialize)]
-struct SiriMessage {
+struct Siri {
     #[serde(rename = "ResponseTimestamp")]
-    response_timestamp: String,
+	response_timestamp: String,
     #[serde(rename = "VehicleMonitoringDelivery")]
-    vehicle_monitoring_delivery: VehicleMonitoringDelivery,
+	vehicle_monitoring_delivery: VehicleMonitoringDelivery,
 }
 
 #[derive(Deserialize)]
 struct VehicleMonitoringDelivery {
     #[serde(rename = "ResponseTimestamp")]
-    response_timestamp: String,
+	response_timestamp: String,
     #[serde(rename = "ValidUntil")]
-    valid_until: String,
+	valid_until: String,
     #[serde(rename = "VehicleActivity")]
-    vehicle_activity: VehicleActivity,
+	vehicle_activity: VehicleActivity,
 }
 
 #[derive(Deserialize)]
 struct VehicleActivity {
     #[serde(rename = "RecordedAtTime")]
-    time_recorded: String,
+	recorded_at_time: String,
     #[serde(rename = "MonitoredVehicleJourney")]
-    monitored_vehicles: Vec<MonitoredVehicleJourney>,
+	monitored_vehicle_journey: Vec<MonitoredVehicleJourney>,
 }
 
 #[derive(Deserialize)]
 struct MonitoredVehicleJourney {
     #[serde(rename = "LineRef")]
-    route_number: String,
+	line_ref: String,
     #[serde(rename = "DirectionRef")]
-    direction_name: String,
+	direction_ref: String,
     #[serde(rename = "FramedVehicleJourneyRef")]
-    framed_vehicle_journey_refs: FramedVehicleJourneyRef,
+	framed_vehicle_journey_ref: FramedVehicleJourneyRef,
     #[serde(rename = "PublishedLineName")]
-    route_name: String,
+	published_line_name: String,
     #[serde(rename = "OriginRef")]
-    origin_ref: u32,
+	origin_ref: u32,
     #[serde(rename = "DestinationRef")]
-    destination_ref: u32,
+	destination_ref: u32,
     #[serde(rename = "Monitored")]
-    monitored: bool,
+	monitored: bool,
     #[serde(rename = "VehicleLocation")]
-    vehicle_location: VehicleLocation,
+	vehicle_location: VehicleLocation,
     #[serde(rename = "ProgressRate")]
-    progress_rate: u8,
+	progress_rate: u32,
     #[serde(rename = "CourseOfJourneyRef")]
-    course_of_journey_ref: u32,
+	course_of_journey_ref: u32,
     #[serde(rename = "VehicleRef")]
-    vehicle_number: u32,
+	vehicle_ref: u32,
     #[serde(rename = "Extensions")]
-    extensions: Extensions,
+	extensions: Extensions,
 }
 
 #[derive(Deserialize)]
 struct FramedVehicleJourneyRef {
-    #[serde(rename = "DateFrameRef")]
-    data_frame_ref: String,
+    #[serde(rename = "DataFrameRef")]
+	data_frame_ref: String,
     #[serde(rename = "DatedVehicleJourneyRef")]
-    dated_vehicle_journey_ref: u32,
+	dated_vehicle_journey_ref: u32,
 }
 
 #[derive(Deserialize)]
 struct VehicleLocation {
     #[serde(rename = "Longitude")]
-    longitude: f32,
+	longitude: f32,
     #[serde(rename = "Latitude")]
-    latitude: f32,
+	latitude: f32,
 }
 
 #[derive(Deserialize)]
 struct Extensions {
     #[serde(rename = "LastGPSFix")]
-    last_gps_fix: String,
+	last_gpsfix: String,
     #[serde(rename = "Scheduled")]
-    scheduled: bool,
+	scheduled: bool,
     #[serde(rename = "Bearing")]
-    bearing: f32,
+	bearing: f32,
     #[serde(rename = "Speed")]
-    speed: f32,
+	speed: f32,
     #[serde(rename = "DestinationName")]
-    destination_name: String,
+	destination_name: String,
 }
 
-fn parse_data(data: String) -> Result<SiriMessage, Box<dyn std::error::Error + Send + Sync>> {
-    let data: SiriMessage = from_str(&data)?;
+fn parse_data(data: String) -> Result<Siri, Box<dyn std::error::Error + Send + Sync>> {
+    let data: Siri = from_str(&data)?;
     Ok(data)
 }
 
@@ -144,5 +145,7 @@ mod tests {
             </Siri>"#,
         );
         println!("{}", data);
+        let data = parse_data(data);
+        assert!(data.is_ok());
     }
 }
