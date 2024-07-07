bdf8f0c24ed20f030939215516a09448304db58e
Nebula3lem123 <whitecottonjacob00@gmail.com>
2024-07-06 21:26:59 -0600

    rewrite siri structs

--- src/lib.rs
+++ src/lib.rs
@@ -9,89 +9,108 @@ pub async fn get_gtfs_rt() -> Result<gtfs_rt::FeedMessage, Box<dyn std::error::E
     )))
 }
 
-
 #[derive(Deserialize, Clone)]
-struct Siri {
-    #[serde(rename = "ResponseTimestamp")]
-	response_timestamp: String,
-    #[serde(rename = "VehicleMonitoringDelivery")]
-	vehicle_monitoring_delivery: VehicleMonitoringDelivery,
+struct ServiceDelivery {
+	VehicleMonitoringDelivery: VehicleMonitoringDelivery,
+	ResponseTimestamp: String,		//time
 }
 
 #[derive(Deserialize, Clone)]
 struct VehicleMonitoringDelivery {
-    #[serde(rename = "ResponseTimestamp")]
-	response_timestamp: String,
-    #[serde(rename = "ValidUntil")]
-	valid_until: String,
-    #[serde(rename = "VehicleActivity")]
-	vehicle_activity: VehicleActivity,
+	VehicleActivity: Option<Vec<VehicleActivity>>,
+	ValidUntil: String, 			//time
+	ResponseTimestamp: String, 		//time
 }
 
 #[derive(Deserialize, Clone)]
 struct VehicleActivity {
-    #[serde(rename = "RecordedAtTime")]
-	recorded_at_time: String,
-    #[serde(rename = "MonitoredVehicleJourney")]
-	monitored_vehicle_journey: Vec<MonitoredVehicleJourney>,
+	MonitoredVehicleJourney: MonitoredVehicleJourney,
+	RecordedAtTime: String,			//time
 }
 
 #[derive(Deserialize, Clone)]
 struct MonitoredVehicleJourney {
-    #[serde(rename = "LineRef")]
-	line_ref: String,
-    #[serde(rename = "DirectionRef")]
-	direction_ref: String,
-    #[serde(rename = "FramedVehicleJourneyRef")]
-	framed_vehicle_journey_ref: FramedVehicleJourneyRef,
-    #[serde(rename = "PublishedLineName")]
-	published_line_name: String,
-    #[serde(rename = "OriginRef")]
-	origin_ref: u32,
-    #[serde(rename = "DestinationRef")]
-	destination_ref: u32,
-    #[serde(rename = "Monitored")]
-	monitored: bool,
-    #[serde(rename = "VehicleLocation")]
-	vehicle_location: VehicleLocation,
-    #[serde(rename = "ProgressRate")]
-	progress_rate: u32,
-    #[serde(rename = "CourseOfJourneyRef")]
-	course_of_journey_ref: u32,
-    #[serde(rename = "VehicleRef")]
-	vehicle_ref: u32,
-    #[serde(rename = "Extensions")]
-	extensions: Extensions,
-}
-
-#[derive(Deserialize, Clone)]
-struct FramedVehicleJourneyRef {
-    #[serde(rename = "DataFrameRef")]
-	data_frame_ref: String,
-    #[serde(rename = "DatedVehicleJourneyRef")]
-	dated_vehicle_journey_ref: u32,
-}
-
-#[derive(Deserialize, Clone)]
-struct VehicleLocation {
-    #[serde(rename = "Longitude")]
-	longitude: f32,
-    #[serde(rename = "Latitude")]
-	latitude: f32,
+	Extensions: Extensions,
+	LineRef: LineRef,
+	FramedVehicleJourneyRef: FramedVehicleJourneyRef,
+	PublishedLineName: Vec<PublishedLineName>,
+	DirectionName: Vec<DirectionName>,
+	OriginRef: OriginRef,
+	DestinationRef: DestinationRef,
+	DestinationName: Vec<DestinationName>,
+	Monitored: bool,
+	VehicleLocation: VehicleLocation,
+	Bearing: f32,
+	ProgressStatus: Vec<ProgressStatus>,
+	CourseOfJourneyRef: CourseOfJourneyRef,
+	VehicleRef: VehicleRef,
 }
 
 #[derive(Deserialize, Clone)]
 struct Extensions {
-    #[serde(rename = "LastGPSFix")]
-	last_gpsfix: String,
-    #[serde(rename = "Scheduled")]
-	scheduled: bool,
-    #[serde(rename = "Bearing")]
-	bearing: f32,
-    #[serde(rename = "Speed")]
-	speed: f32,
-    #[serde(rename = "DestinationName")]
-	destination_name: String,
+	LastGPSFix: String,			//time
+}
+
+#[derive(Deserialize, Clone)]
+struct LineRef {
+	value: String,
+}
+
+#[derive(Deserialize, Clone)]
+struct FramedVehicleJourneyRef {
+	DataFrameRef: DataFrameRef,
+	DatedVehicleJourneyRef: i32,
+}
+
+#[derive(Deserialize, Clone)]
+struct PublishedLineName {
+	value: String,
+}
+
+#[derive(Deserialize, Clone)]
+struct DirectionName {
+	value: String,
+}
+
+#[derive(Deserialize, Clone)]
+struct OriginRef {
+	value: i32,				//origin station code
+}
+
+#[derive(Deserialize, Clone)]
+struct DestinationRef {
+	value: i32,				//destination station code
+}
+
+#[derive(Deserialize, Clone)]
+struct DestinationName {
+	value: String,				//destination station name
+}
+
+#[derive(Deserialize, Clone)]
+struct VehicleLocation {
+	latitude: f32,
+	longitude: f32,
+}
+
+#[derive(Deserialize, Clone)]
+struct ProgressStatus {
+	value: String,
+}
+
+#[derive(Deserialize, Clone)]
+struct CourseOfJourneyRef {
+	value: i32,
+}
+
+#[derive(Deserialize, Clone)]
+struct VehicleRef {
+	value: u16,
+}
+
+#[derive(Deserialize, Clone)]
+struct DataFrameRef {
+    value: String,
 }
 
 // fn parse_data(data: String) -> Result<Siri, Box<dyn std::error::Error + Send + Sync>> {
